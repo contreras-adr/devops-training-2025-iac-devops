e17da93655847cb4e85acadd1624ef0e35634c81
adrian.contreras <adrian.contreras@scalian.com>
2025-04-08 17:33:17 +0200

    add common jenkins functions

---
+++ jenkins/functions.groovy
@@ -0,0 +1,8 @@
+def getLastGitTag() {
+    sh "git tag --sort version:refname | head -n 1 > version.tmp"
+    String tag = readFile 'version.tmp'
+    echo "Branch: ${scm.branches[0].name}"
+    def result = tag ?: "0.0.1-Snapshot"
+    echo "Tag, ${result}." 
+    return result
+}
